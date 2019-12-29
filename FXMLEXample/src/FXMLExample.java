5db1fa97c6dcaecb92f7c96ddeae2760866f2b73
shengexing <931709131@qq.com>
2019-12-29 21:10:39 +0800

    1.学习内容：DOC-01-06 使用FXML进行用户界面设计（http://www.javafxchina.net/blog/2015/07/doc01_fxml/） 2.修改描述：使用fxml文件写JavaFX的界面，使用java或js控制界面动作，使用css设计界面样式 3.修改人：shengexing 4.修改时间：2019-12-29

---
+++ FXMLEXample/src/FXMLExample.java
@@ -0,0 +1,35 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class FXMLExample extends Application {
+    /**
+     * The main entry point for all JavaFX applications.
+     * The start method is called after the init method has returned,
+     * and after the system is ready for the application to begin running.
+     *
+     * <p>
+     * NOTE: This method is called on the JavaFX Application Thread.
+     * </p>
+     *
+     * @param primaryStage the primary stage for this application, onto which
+     *                     the application scene can be set. The primary stage will be embedded in
+     *                     the browser if the application was launched as an applet.
+     *                     Applications may create other stages, if needed, but they will not be
+     *                     primary stages and will not be embedded in the browser.
+     */
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("fxml_example_js.fxml"));
+        Scene scene = new Scene(root, 300, 275);
+        primaryStage.setTitle("FXML Welcome");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
